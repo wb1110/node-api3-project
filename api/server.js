5738d8aaa8257aa1f14619240a91c599e3432c7b
wb1110 <strengthcoachwb@gmail.com>
2022-01-12 14:26:57 -0600

    starting pieces

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const server = express();
+server.use(express.json());
+const usersRouter = require('./users/users-router');
+server.use('', usersRouter);
 
 // remember express by default cannot parse JSON in request bodies
 
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // require your server and launch it
+const server = require('./api/server')
+const port = 9000
+
+server.listen(port)
